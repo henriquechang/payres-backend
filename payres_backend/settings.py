6447236296fa4e54cb65dcbdcd8802e2f99bf07a
henriquechang <henrique.queiroz@cronapp.io~>
2019-10-08 20:13:55 -0300

    corrigindo url da cors whitelist

--- payres_backend/settings.py
+++ payres_backend/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
-    'payres-backend.herokuapp.com'
+    'https://payres-backend.herokuapp.com'
 )
 
 ROOT_URLCONF = 'payres_backend.urls'
